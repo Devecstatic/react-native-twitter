0b89a039024cba3943cdb9a114102873018fd549
Ethan Lipnik <ethanlipnik@gmail.com>
2020-01-03 21:38:09 -0500

    Changed url stuff

--- src/oauth/auth.js
+++ src/oauth/auth.js
@@ -1,15 +1,17 @@
-import {Linking} from 'react-native';
+import { Linking } from 'react-native';
 
-import URLSearchParams from 'url-search-params';
+import { URLSearchParams } from 'whatwg-url';
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 import request from './request';
-import {query} from '../util';
+import { query } from '../util';
 
 function getRequestToken(tokens, callbackUrl, accessType) {
   const method = 'POST';
   const url = 'https://api.twitter.com/oauth/request_token';
-  const body = accessType ? {x_auth_access_type: accessType} : {};
-  return request(tokens, url, {method, body}, {oauth_callback: callbackUrl})
+  const body = accessType ? { x_auth_access_type: accessType } : {};
+  return request(tokens, url, { method, body }, { oauth_callback: callbackUrl })
     .then(response => response.text())
     .then((text) => {
       const params = new URLSearchParams(text);
@@ -21,16 +23,16 @@ function getRequestToken(tokens, callbackUrl, accessType) {
 }
 
 function getAccessToken(
-  {consumerKey, consumerSecret, requestToken, requestTokenSecret},
+  { consumerKey, consumerSecret, requestToken, requestTokenSecret },
   oauthVerifier,
 ) {
   const method = 'POST';
   const url = 'https://api.twitter.com/oauth/access_token';
   return request(
-    {consumerKey, consumerSecret, oauthToken: requestToken, oauthTokenSecret: requestTokenSecret},
+    { consumerKey, consumerSecret, oauthToken: requestToken, oauthTokenSecret: requestTokenSecret },
     url,
-    {method},
-    {oauth_verifier: oauthVerifier},
+    { method },
+    { oauth_verifier: oauthVerifier },
   )
     .then(response => response.text())
     .then((text) => {
@@ -46,7 +48,7 @@ function getAccessToken(
 
 const verifierDeferreds = new Map();
 
-Linking.addEventListener('url', ({url}) => {
+Linking.addEventListener('url', ({ url }) => {
   const params = new URLSearchParams(url.split('?')[1]);
   if (params.has('oauth_token') && verifierDeferreds.has(params.get('oauth_token'))) {
     const verifierDeferred = verifierDeferreds.get(params.get('oauth_token'));
@@ -62,23 +64,23 @@ Linking.addEventListener('url', ({url}) => {
 export default async function auth(
   tokens,
   callbackUrl,
-  {accessType, forSignIn = false, forceLogin = false, screenName = ''} = {},
+  { accessType, forSignIn = false, forceLogin = false, screenName = '' } = {},
 ) {
   const usePin = typeof callbackUrl.then === 'function';
-  const {requestToken, requestTokenSecret} = await getRequestToken(
+  const { requestToken, requestTokenSecret } = await getRequestToken(
     tokens,
     usePin ? 'oob' : callbackUrl,
     accessType,
   );
   Linking.openURL(`https://api.twitter.com/oauth/${forSignIn ? 'authenticate' : 'authorize'}?${
-    query({oauth_token: requestToken, force_login: forceLogin, screen_name: screenName})
-  }`);
+    query({ oauth_token: requestToken, force_login: forceLogin, screen_name: screenName })
+    }`);
   return getAccessToken(
-    {...tokens, requestToken, requestTokenSecret},
+    { ...tokens, requestToken, requestTokenSecret },
     await (
       usePin ?
         callbackUrl :
-        new Promise((resolve, reject) => {verifierDeferreds.set(requestToken, {resolve, reject});})
+        new Promise((resolve, reject) => { verifierDeferreds.set(requestToken, { resolve, reject }); })
     ),
   );
 }
